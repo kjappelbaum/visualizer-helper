82188acec2bd1640d898ff715a75d5ad2d260746
Luc Patiny <luc@patiny.com>
2015-10-07 19:30:32 +0200

    Update

--- util/trackOptions.js
+++ util/trackOptions.js
@@ -1,18 +1,23 @@
-function init(cookieName) {
-    var options = {};
-    try {
-        options = JSON.parse(window.localStorage.getItem(cookieName)) || {};
-    } catch(e) {
-        console.log(e);
-    };
-    API.createData('options', options);
+define(function () {
 
-    var data = require('src/util/versioning').getData();
-    data.onChange(function (evt) {
-        if(evt.jpath.length==1 && evt.jpath[0]=='options') {
-            localStorage.setItem(cookieName, JSON.stringify(evt.target));
+    function init(cookieName) {
+        var options = {};
+        try {
+            options = JSON.parse(window.localStorage.getItem(cookieName)) || {};
+        } catch (e) {
+            console.log(e);
         }
-    });
-};
+        ;
+        API.createData('options', options);
 
-exports.init = init;
+        var data = require('src/util/versioning').getData();
+        data.onChange(function (evt) {
+            if (evt.jpath.length == 1 && evt.jpath[0] == 'options') {
+                localStorage.setItem(cookieName, JSON.stringify(evt.target));
+            }
+        });
+
+        return init;
+    }
+});
+
