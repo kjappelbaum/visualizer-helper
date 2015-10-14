9bac0b45679268938d656ad804dbb93a456eb975
Luc Patiny <luc@patiny.com>
2015-10-14 16:44:02 +0200

    Add method watchAnswers

---
+++ util/watchAnswers.js
@@ -0,0 +1,30 @@
+/*
+In the general preferences you should put something like:
+require(['TrackOptions'], function(TrackOptions) {
+    TrackOptions('massOptions');
+})
+*/
+
+define([], function () {
+    function watchAnswers(cookieName, exercises) {
+        var myAnswers = JSON.parse(window.localStorage.getItem(cookieName) || "{}");
+
+        for (var i=0; i<exercises.length; i++) {
+            var exercise=exercises[i];
+            var myAnswer=myAnswers[exercise.id];
+            if (myAnswer) {
+                exercise.myResult=myAnswer;
+            }
+        }
+
+        exercises.onChange(function (evt) {
+            var target=evt.target.__parent;
+            if (target) {
+                myAnswers[target.id]=target.myResult;
+                window.localStorage.setItem(cookieName, JSON.stringify(myAnswers));
+            }
+        });
+    }
+    return watchAnswers;
+});
+
