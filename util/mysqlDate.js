fdb6129e06436a30a5febd6900832109bf6eb76d
lpatiny <lpatiny@users.noreply.github.com>
2015-10-17 19:17:09 +0200

    Update mysqlDate.js

--- util/mysqlDate.js
+++ util/mysqlDate.js
@@ -1,3 +1,6 @@
+// returns a Mysql formatted date, very practical to get a string with dthe date
+
+
 define([], function () {
 
   function twoDigits(d) {
@@ -6,11 +9,11 @@ define([], function () {
     return d.toString();
   }
 
-  function convert = function(date) {
+  function create(date) {
     if (!date) date=new Date();
     return date.getUTCFullYear() + "-" + twoDigits(1 + date.getUTCMonth()) + "-" + twoDigits(date.getUTCDate()) + " " + twoDigits(date.getUTCHours()) + ":" + twoDigits(date.getUTCMinutes()) + ":" + twoDigits(date.getUTCSeconds());
   };
 
-  return convert;
+  return create;
 
 });
