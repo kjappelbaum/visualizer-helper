49ac460a267ee8fd09f700eb1ea09651cb60036c
Daniel Kostro <kostro.d@gmail.com>
2016-03-18 15:04:42 +0100

    fix trackArray

--- util/trackArray.js
+++ util/trackArray.js
@@ -1,15 +1,17 @@
-define(['src/util/api'], function (API) {
+define(['src/util/api', 'lodash'], function (API, _) {
 
     function track(cookieName, defaultValue) {
         if (API.getData(cookieName)) return;
         var options = [];
         try {
             options = JSON.parse(window.localStorage.getItem(cookieName)) || [];
-            if (defaultValue) options = $.extend(true, defaultValue, options);
+            if(!Array.isArray(options)) throw new Error('TrackArray expected an array in local storage');
+            options = options.concat(defaultValue);
+            options = _.uniqWith(options, _.isEqual);
         } catch (e) {
             console.error(e);
         }
-        ;
+
         API.createData(cookieName, options);
 
         
