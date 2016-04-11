4c2a688ef8484f656bcca16281005149b86f8061
Daniel Kostro <kostro.d@gmail.com>
2016-04-11 16:20:34 +0200

    trackArray: better use of change listener

--- util/trackArray.js
+++ util/trackArray.js
@@ -1,27 +1,23 @@
+'use strict';
+
 define(['src/util/api', 'lodash'], function (API, _) {
 
-    function track(cookieName, defaultValue) {
-        if (API.getData(cookieName)) return;
-        var options = [];
+    function track(localName, defaultValue) {
+        if (API.getData(localName)) return;
+        var localValue = [];
         try {
-            options = JSON.parse(window.localStorage.getItem(cookieName)) || [];
-            if(!Array.isArray(options)) throw new Error('TrackArray expected an array in local storage');
-            options = options.concat(defaultValue);
-            options = _.uniqWith(options, _.isEqual);
+            localValue = JSON.parse(window.localStorage.getItem(localName)) || [];
+            if(!Array.isArray(localValue)) throw new Error('TrackArray expected an array in local storage');
+            localValue = localValue.concat(defaultValue);
+            localValue = _.uniqWith(localValue, _.isEqual);
         } catch (e) {
             console.error(e);
         }
 
-        API.createData(cookieName, options);
-
-        
-        var data = require('src/util/versioning').getData();
-        data.onChange(function (evt) {
-            // If any part of the object changes, we resave the entire object
-            if (evt.jpath[0] == cookieName) {
-                var target = API.getData(cookieName);
-                localStorage.setItem(cookieName, JSON.stringify(target));
-            }
+        API.createData(localName, localValue).then(function(data) {
+            data.onChange(function() {
+                localStorage.setItem(localName, JSON.stringify(data));
+            });
         });
     }
 
