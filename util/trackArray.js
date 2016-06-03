b6ba213751c34be571a41657b3f2471451322c8a
Luc Patiny <luc@patiny.com>
2016-06-03 17:11:04 +0200

    Add comparator for TrackArray

--- util/trackArray.js
+++ util/trackArray.js
@@ -2,14 +2,15 @@
 
 define(['src/util/api', 'lodash'], function (API, _) {
 
-    function track(localName, defaultValue) {
+    function track(localName, defaultValue, comparator) {
         if (API.getData(localName)) return;
+        var comparator = comparator || _.isEqual;
         var localValue = [];
         try {
             localValue = JSON.parse(window.localStorage.getItem(localName)) || [];
             if(!Array.isArray(localValue)) throw new Error('TrackArray expected an array in local storage');
             localValue = localValue.concat(defaultValue);
-            localValue = _.uniqWith(localValue, _.isEqual);
+            localValue = _.uniqWith(localValue, comparator);
         } catch (e) {
             console.error(e);
         }
