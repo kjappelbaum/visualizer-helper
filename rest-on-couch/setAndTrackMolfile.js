3b1ddd6b6cedf2e421b596cf48dd480c3286da51
Luc Patiny <lpatiny@users.noreply.github.com>
2016-08-03 17:30:48 +0200

    Update setAndTrackMolfile.js

--- rest-on-couch/setAndTrackMolfile.js
+++ rest-on-couch/setAndTrackMolfile.js
@@ -1,59 +1,64 @@
 
 require(['src/util/api'], function(API) {
     if (typeof IframeBridge != 'undefined') {
-    require(['Roc','Sample'], function(Roc, Sample) {
-        IframeBridge.onMessage(onMessage);
-        IframeBridge.ready();
-        function onMessage(data) {
-            if(data.type === 'tab.data' || data.type === 'tab.focus') {
-                var roc = new Roc({
-                    url: data.message.couchUrl,
-                    database: data.message.database
-                });
-                var sample = new Sample(roc, data.message.uuid, 'sample', {track:false});
+        require(['Roc','Sample'], function(Roc, Sample) {
+            IframeBridge.onMessage(onMessage);
+            IframeBridge.ready();
+            function onMessage(data) {
+                if(data.type === 'tab.data' || data.type === 'tab.focus') {
+                    var roc = new Roc({
+                        url: data.message.couchUrl,
+                        database: data.message.database
+                    });
+                    var sample = new Sample(roc, data.message.uuid, 'sample', {track:false});
+                    // TODO should only be executed if there is a molfile ... but how to check ?????
+                    API.doAction('molfileInitialized');
+                }
+                
             }
-        }
-    });
-} else {
-    // we retrieve the cache if it exists
-    var externalInfo = JSON.parse(window.localStorage.getItem('external_cache') || "{}");
-    var smiles = externalInfo.smiles;
-    var molfile = externalInfo.molfile;
-    var nmr = API.createData('nmr',[]);    
-    
-    require(["uri/URI"], function(URI) {
-        var uri = new URI(document.location.href);
-        var search = uri.search(true);
-        if (search.smiles) {
-            smiles=search.smiles;
-        }
+        });
+    } else {
+        // we retrieve the cache if it exists
+        var externalInfo = JSON.parse(window.localStorage.getItem('external_cache') || "{}");
+        var smiles = externalInfo.smiles;
+        var molfile = externalInfo.molfile;
+        var nmr = API.createData('nmr',[]);    
         
-        if (molfile) {
-            require(["OCLE"], function (OCLE) {
-                var molecule=OCLE.Molecule.fromMolfile(molfile);
-                API.createData('molfile', molecule.toMolfile());
-            });
-        } else if(smiles) {
-            require(["OCLE"], function (OCLE) {
-                var molecule=OCLE.Molecule.fromSmiles(smiles);
-                API.createData('molfile', molecule.toMolfile());
-            });
-        } else {
-            molfile=window.localStorage.getItem('molfile');
+        require(["uri/URI"], function(URI) {
+            var uri = new URI(document.location.href);
+            var search = uri.search(true);
+            if (search.smiles) {
+                smiles=search.smiles;
+            }
+            
             if (molfile) {
-        		API.createData('molfile', molfile);
-        	} else {
-        	    API.createData('molfile', '');
-        	}
-        }
-     })
-    
-    var data = require('src/util/versioning').getData();
-    data.onChange(function (evt) {
-        if(evt.jpath.length==1 && evt.jpath[0]=='molfile') {
-            localStorage.setItem('molfile', evt.target.get());
-        }
-    });
-}
+                require(["OCLE"], function (OCLE) {
+                    var molecule=OCLE.Molecule.fromMolfile(molfile);
+                    API.createData('molfile', molecule.toMolfile());
+                    API.doAction('molfileInitialized');
+                });
+            } else if(smiles) {
+                require(["OCLE"], function (OCLE) {
+                    var molecule=OCLE.Molecule.fromSmiles(smiles);
+                    API.createData('molfile', molecule.toMolfile());
+                    API.doAction('molfileInitialized');
+                });
+            } else {
+                molfile=window.localStorage.getItem('molfile');
+                if (molfile) {
+            		API.createData('molfile', molfile);
+            		API.doAction('molfileInitialized');
+            	} else {
+            	    API.createData('molfile', '');
+            	}
+            }
+         })
+        
+        var data = require('src/util/versioning').getData();
+        data.onChange(function (evt) {
+            if(evt.jpath.length==1 && evt.jpath[0]=='molfile') {
+                localStorage.setItem('molfile', evt.target.get());
+            }
+        });
+    }
 })
-
