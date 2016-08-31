fafaef5c3dfbffd9929c8d65ad460f1b89ee5929
Daniel Kostro <kostro.d@gmail.com>
2016-08-31 13:30:25 +0200

    ChemEquilibrium: add chart utility

---
+++ ChemEquilibrium/chart.js
@@ -0,0 +1,39 @@
+'use strict';
+
+define(['src/util/Color'], function (Color) {
+    return {
+        getChart(x, y, options) {
+            if(x.length !== y.length || y.length === 0) {
+                throw new Error('Invalid data length');
+            }
+            var chart = {
+                data: [],
+                axis: [{
+                    label: options.xLabel || '',
+                }, {
+                    label: options.yLabel || '',
+                }]
+            };
+
+            var colors = Color.getDistinctColors(species.length);
+
+            var species = Object.keys(y[0]);
+            for (var i = 0; i < species.length; i++) {
+                var data = {};
+                chart.data.push(data);
+                data.y = y.map(function (y) {
+                    return y[species[i]];
+                });
+                data.x = x;
+                data.label = species[i];
+                data.xAxis = 0;
+                data.yAxis = 1;
+                data.defaultStyle = {
+                    lineColor: colors[i],
+                    lineWidth: 1
+                }
+            }
+            return chart;
+        }
+    }
+});
