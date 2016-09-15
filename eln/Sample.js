b12505b7f5e02d6ffcb832f12d36b45920322919
Luc Patiny <luc@patiny.com>
2016-09-15 14:04:11 +0200

    Fix bug

--- eln/Sample.js
+++ eln/Sample.js
@@ -10,7 +10,7 @@ define([
     'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js',
     'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js',
     'https://www.lactame.com/github/cheminfo-js/visualizer-helper/7f9c4d2c296389ed263a43826bafeba1164d13de/rest-on-couch/Roc.js'
-], function (Nmr1dManager, ExpandableMolecule, API, UI, CCE, elnPlugin, Roc) {
+], function (ExpandableMolecule, Nmr1dManager, API, UI, CCE, elnPlugin, Roc) {
 
     var defaultOptions = {
         varName: 'sample',
