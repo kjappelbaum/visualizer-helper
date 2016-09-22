0dd025e3f4f8cb3939ed6ac8083ccf569d270f10
andres castillo <andcastillo@gmail.com>
2016-09-22 14:18:46 -0500

    Update libs.js
    sdv v0.1.15

--- eln/libs.js
+++ eln/libs.js
@@ -3,7 +3,7 @@
 
 define([
     'https://www.lactame.com/lib/openchemlib-extended/1.11.0/openchemlib-extended.js',
-    'https://www.lactame.com/lib/sdv/0.1.14/sdv.js',
+    'https://www.lactame.com/lib/sdv/0.1.15/sdv.js',
     'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js',
     'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js',
     'https://www.lactame.com/github/cheminfo-js/visualizer-helper/7f9c4d2c296389ed263a43826bafeba1164d13de/rest-on-couch/Roc.js'
