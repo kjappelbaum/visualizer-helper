771a226f0fac093d527e29c927616bb6cd710e4b
Luc Patiny <luc@patiny.com>
2016-09-26 14:49:35 +0200

    Change openchemlib-extended to 1.12.1

--- eln/libs.js
+++ eln/libs.js
@@ -2,7 +2,7 @@
 
 
 define([
-    'https://www.lactame.com/lib/openchemlib-extended/1.11.0/openchemlib-extended.js',
+    'https://www.lactame.com/lib/openchemlib-extended/1.12.1/openchemlib-extended.js',
     'https://www.lactame.com/lib/sdv/0.1.17/sdv.js',
     'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js',
     'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js',
