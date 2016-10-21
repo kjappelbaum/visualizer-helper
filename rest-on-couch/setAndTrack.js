414a1afa909be83ac6f0a2cde05e23ccc53c372a
Luc Patiny <lpatiny@users.noreply.github.com>
2016-10-21 13:48:01 +0200

    Update setAndTrack.js

--- rest-on-couch/setAndTrack.js
+++ rest-on-couch/setAndTrack.js
@@ -12,50 +12,55 @@ define([
         IframeBridge.onMessage(onMessage);
         IframeBridge.ready();
         function onMessage(data) {
-            if(data.type === 'tab.data' || data.type === 'tab.focus') {
-                var roc = new Roc(data.message.couchDB);
-                var sample = new Sample(roc, data.message.uuid, data.message.couchDB.kind, {track:false});
+            if (data.type === 'tab.data') {
+                if (data.message && data.message.uuid) {
+                    var roc = new Roc(data.message.couchDB);
+                    var sample = new Sample(roc, data.message.uuid, data.message.couchDB.kind, {track:false});   
+                } else {
+                    fromCookie();
+                }
             }
-
+            if (data.type === 'tab.focus') {};
         }
     } else {
-        // we retrieve the cache if it exists
-        var externalInfo = JSON.parse(window.localStorage.getItem('external_cache') || "{}");
-        var smiles = externalInfo.smiles;
-        var molfile = externalInfo.molfile;
-        API.createData('nmr',[]);
-        API.createData('mass',[]);
-        API.createData('ir',[]);
-
-        var uri = new URI(document.location.href);
-        var search = uri.search(true);
-        if (search.smiles) {
-            smiles=search.smiles;
-        }
-
-        if (molfile) {
-            var molecule=OCLE.Molecule.fromMolfile(molfile);
-            API.createData('molfile', molecule.toMolfile());
-        } else if(smiles) {
-            var molecule=OCLE.Molecule.fromSmiles(smiles);
-            API.createData('molfile', molecule.toMolfile());
-        } else {
-            molfile=window.localStorage.getItem('molfile');
-            if (molfile) {
-                API.createData('molfile', molfile);
-            } else {
-                API.createData('molfile', '');
-            }
-        }
-
-        var data = Versioning.getData();
-        data.onChange(function (evt) {
-            if(evt.jpath.length==1 && evt.jpath[0]=='molfile') {
-                localStorage.setItem('molfile', evt.target.get());
-            }
-        });
+        fromCookie();
     }
 });
 
+function fromCookie() {
+    // we retrieve the cache if it exists
+    var externalInfo = JSON.parse(window.localStorage.getItem('external_cache') || "{}");
+    var smiles = externalInfo.smiles;
+    var molfile = externalInfo.molfile;
+    API.createData('nmr',[]);
+    API.createData('mass',[]);
+    API.createData('ir',[]);
 
+    var uri = new URI(document.location.href);
+    var search = uri.search(true);
+    if (search.smiles) {
+        smiles=search.smiles;
+    }
 
+    if (molfile) {
+        var molecule=OCLE.Molecule.fromMolfile(molfile);
+        API.createData('molfile', molecule.toMolfile());
+    } else if(smiles) {
+        var molecule=OCLE.Molecule.fromSmiles(smiles);
+        API.createData('molfile', molecule.toMolfile());
+    } else {
+        molfile=window.localStorage.getItem('molfile');
+        if (molfile) {
+            API.createData('molfile', molfile);
+        } else {
+            API.createData('molfile', '');
+        }
+    }
+
+    var data = Versioning.getData();
+    data.onChange(function (evt) {
+        if(evt.jpath.length==1 && evt.jpath[0]=='molfile') {
+            localStorage.setItem('molfile', evt.target.get());
+        }
+    });
+}
