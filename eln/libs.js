6923df07702a281974315d4949ee87f2c7d8e7b9
Daniel Kostro <kostro.d@gmail.com>
2016-11-22 15:40:37 +0100

    Sample: update Roc

--- eln/libs.js
+++ eln/libs.js
@@ -6,7 +6,7 @@ define([
     'https://www.lactame.com/lib/sdv/0.1.20/sdv.js',
     'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js',
     'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js',
-    'https://www.lactame.com/github/cheminfo-js/visualizer-helper/dc047baf7264d831010221d1e89e0a082e285760/rest-on-couch/Roc.js'
+    'https://www.lactame.com/github/cheminfo-js/visualizer-helper/fbbf28649f34169eee925d920d3a5161676d5bb0/rest-on-couch/Roc.js'
 ], function (OCLE, SD, CCE, elnPlugin, Roc) {
 
     return {
