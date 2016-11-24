6c048ac5cd2a1f7e46213a3ecae5f0bc63e024e2
Daniel Kostro <kostro.d@gmail.com>
2016-11-24 16:14:21 +0100

    Printer: return requests' body

--- eln/stock/Printer.js
+++ eln/stock/Printer.js
@@ -8,39 +8,39 @@ define(['superagent', 'uri/URI'], function (superagent, URI) {
 
         async print(format, data) {
             const url = new URI(this.url).segment('print').href();
-            return await superagent
+            return (await superagent
                 .post(url)
                 .send({
                     format, data
-                });
+                })).body;
         }
 
         async ports() {
             const url = new URI(this.url).segment('ports').href();
-            return await superagent.get(url);
+            return (await superagent.get(url)).body;
         }
 
         async deleteFormat(format) {
             const url = new URI(this.url).segment('db/format').segmentCoded(format).href();
-            return await superagent.del(url);
+            return (await superagent.del(url)).body;
         }
 
         async saveFormat(key, value) {
             const url = new URI(this.url).segment('db/format').href();
-            return await superagent.put(url).send({key, value})
+            return (await superagent.put(url).send({key, value})).body;
         }
 
         async getFormat(format) {
             const url = new URI(this.url).segment('db/format').segmentCoded(format).href();
-            return await superagent.get(url);
+            return (await superagent.get(url)).body;
         }
 
         async getAllFormats() {
             const url = new URI(this.url).segment('db/format').href();
-            return await superagent.get(url);
+            return (await superagent.get(url)).body;
         }
     }
 
+
     return Printer;
 });
-
