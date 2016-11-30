bbfa3bbb404a4c8901d748e6ac0ac9955d458794
Daniel Kostro <kostro.d@gmail.com>
2016-11-30 11:24:45 +0100

    spectra-data v2.1.0

--- eln/libs.js
+++ eln/libs.js
@@ -3,7 +3,7 @@
 
 define([
     'https://www.lactame.com/lib/openchemlib-extended/1.12.1/openchemlib-extended.js',
-    'https://www.lactame.com/lib/spectra-data/HEAD/spectra-data.js',
+    'https://www.lactame.com/lib/spectra-data/2.1.0/spectra-data.js',
     'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js',
     'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js',
     'https://www.lactame.com/github/cheminfo-js/visualizer-helper/fbbf28649f34169eee925d920d3a5161676d5bb0/rest-on-couch/Roc.js'
