70dcc8b0b6a9d91e71bf7a0ee016b78c3e3bacfe
Michaël Zasso <targos@protonmail.com>
2016-12-12 16:41:14 +0100

    use absolute link to Roc

--- eln/libs.js
+++ eln/libs.js
@@ -4,4 +4,4 @@ export {default as OCLE} from 'https://www.lactame.com/lib/openchemlib-extended/
 export {default as SD} from 'https://www.lactame.com/lib/spectra-data/2.1.0/spectra-data.js';
 export {default as CCE} from 'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js';
 export {default as elnPlugin} from 'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js';
-export {default as Roc} from '../rest-on-couch/Roc';
+export {default as Roc} from 'https://www.lactame.com/github/cheminfo-js/visualizer-helper/fbbf28649f34169eee925d920d3a5161676d5bb0/rest-on-couch/Roc.js';
