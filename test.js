0be65a3f8bc7433d7dc708711fd00824d72046e0
Daniel Kostro <kostro.d@gmail.com>
2017-01-04 16:47:27 +0100

    remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-define([], async function () {
-  async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  console.log(1);
-  await sleep()
-  console.log(2);
-})
