5a9d94123403e7cb11273e709b0e445a302c2ae8
Daniel Kostro <kostro.d@gmail.com>
2017-01-05 10:28:10 +0100

    printer: preprocess sample data

--- eln/stock/printer/Printer.js
+++ eln/stock/printer/Printer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['./PrintServer', './printProcessors'], function (PrintServer, processors) {
+define(['src/util/util', './PrintServer', './printProcessors'], function (Util, PrintServer, processors) {
 
     class Printer {
         constructor(printer, printServer) {
@@ -12,8 +12,28 @@ define(['./PrintServer', './printProcessors'], function (PrintServer, processors
 
         async print(printFormat, data) {
             if(!processors[printFormat.processor]) throw new Error('processor does not exist');
+            processData(printFormat, data);
             const printData = await processors[String(printFormat.processor)].call(null, printFormat, data);
             return await this.printServer.print(this.id, printData);
+        }
+    }
+
+    function processData(printFormat, data) {
+        switch(printFormat.type) {
+            case 'sample': {
+                data.uuidShort = data._id.substring(0,12);
+                data.b64Short = Util.hexToBase64(data.uuidShort);
+                data.line1 = data.$content.general.description.substring(0,60);
+                data.line2 = data.$content.general.description.substring(60,120);
+                data.id = data.$id.join(' ');
+                break;
+            }
+            case 'location': {
+                break;
+            }
+            default: {
+                throw new Error('Printer could not process data');
+            }
         }
     }
 
