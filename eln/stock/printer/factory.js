d07acf0698bf87f051bf9f9a78b3e7aad91b636c
Daniel Kostro <kostro.d@gmail.com>
2017-01-06 07:31:36 +0100

    printer factory

--- eln/stock/printer/factory.js
+++ eln/stock/printer/factory.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import Roc from './Roc';
+import printer from './printer';
 
 module.exports = function(opts, cb) {
     if(typeof opts === 'function') {
@@ -10,10 +10,10 @@ module.exports = function(opts, cb) {
     if(typeof IframeBridge !== 'undefined') {
         IframeBridge.onMessage(function(data) {
             if(data.type === 'tab.data') {
-                if(data.message.couchDB) {
-                    const options = Object.assign({}, data.message.couchDB, opts);
-                    var roc = new Roc(options);
-                    cb(roc, data.message.couchDB);
+                if(data.message.printer && data.message.printer.couchDB) {
+                    const options = Object.assign({}, data.message.printer.couchDB, opts);
+                    var p = printer(options);
+                    cb(p, data.message.printer.couchDB);
                 }
             }
         });
