f43ce98957cc98a83ebf07e2b123ad07890fdc33
Daniel Kostro <kostro.d@gmail.com>
2017-02-06 16:45:50 +0100

    Revert "Change spectra-data to HEAD version for testing"
    This reverts commit d7e7a53122717d69059e17cceb65457c1dff28ef.

--- eln/libs.js
+++ eln/libs.js
@@ -1,7 +1,6 @@
 'use strict';
 
 export {default as OCLE} from 'https://www.lactame.com/lib/openchemlib-extended/1.13.0/openchemlib-extended.js';
-//export {default as SD} from 'https://www.lactame.com/lib/spectra-data/2.1.0/spectra-data.js';
-export {default as SD} from 'http://www.lactame.com/lib/spectra-data/HEAD/spectra-data.js';
+export {default as SD} from 'https://www.lactame.com/lib/spectra-data/2.1.0/spectra-data.js';
 export {default as CCE} from 'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js';
 export {default as elnPlugin} from 'https://www.lactame.com/lib/eln-plugin/0.0.2/eln-plugin.js';
