f2891960e83e7956ca9a603cfcce1b4eb1b43660
Luc Patiny <luc@patiny.com>
2017-05-02 14:33:56 +0200

    Add xyParser

--- spectra-data/loadIRZipXY.js
+++ spectra-data/loadIRZipXY.js
@@ -3,6 +3,7 @@
 async function loadZips(zipURLs, options = {}) {
     const JSZip = await API.require('jszip');
     const superagent = await API.require('superagent');
+    const xyParser = await API.require('https://www.lactame.com/lib/xy-parser/1.3.0/xy-parser.min.js');
     var jszip = new JSZip();
     var spectraDataSet = [];
     for (let zipURL of zipURLs) {
