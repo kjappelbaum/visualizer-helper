d6118b4581d79cf4ee4af6b0626bc0f69f093fd9
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-05-02 15:22:02 +0200

    loadJcampZip added

--- spectra-data/loadJcampZip.js
+++ spectra-data/loadJcampZip.js
@@ -5,7 +5,6 @@ import API from 'src/util/api';
 module.exports = async function loadZips(zipURLs, options = {}) {
     const JSZip = await API.require('jszip');
     const superagent = await API.require('superagent');
-    const xyParser = await API.require('https://www.lactame.com/lib/xy-parser/1.3.0/xy-parser.min.js');
     const SD = await API.require('https://www.lactame.com/lib/spectra-data/3.0.7/spectra-data.min.js');
     var jszip = new JSZip();
     var spectraDataSet = [];
@@ -14,21 +13,14 @@ module.exports = async function loadZips(zipURLs, options = {}) {
             .withCredentials()
             .responseType('blob');
         var zip = await jszip.loadAsync(zipFiles.body);
-        let filesToProcess = Object.keys(zip.files).filter( filename => filename.match(/\.[0-9]+$/));
+        let filesToProcess = Object.keys(zip.files).filter( filename => filename.match(/jdx$/));
         for (const filename of filesToProcess) {
-            let fileData=await zip.files[filename].async('string');
-            let result = xyParser(fileData, {arrayType: 'xxyy'});
-            let spectrum = SD.NMR.fromXY(result[0], result[1], {
-                dataType: 'IR',
-                xUnit: 'waveNumber',
-                yUnit: ''
-            });
+            let jcamp = await zip.files[filename].async('string');
+            let sd = SD.NMR.fromJcamp(jcamp, {});
+            sd.sd.filename = filename.replace(/.jdx$/,'');
             if (options.filter) options.filter(spectrum.sd);
-            spectrum.sd.info = {};
-            spectrum.sd.filename = filename;
             spectraDataSet.push(spectrum);
         }
     }
     return spectraDataSet;
 }
-
