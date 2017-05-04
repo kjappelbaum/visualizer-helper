7a0e2c2e3ae495648f2806ec129a20791895aac3
Luc Patiny <luc@patiny.com>
2017-05-04 16:36:16 +0200

    Add recordInfo for roc records

---
+++ rest-on-couch/recordInfo.js
@@ -0,0 +1,70 @@
+'use strict';
+
+define(['src/util/ui'], function(UI) {
+
+    function recordInfo(record) {
+        let html='';
+        html+=`<style>
+            #ownerInfo {font-size:2em};
+            #ownerInfo h1 {font-size: 30px};
+            #ownerInfo th {text-align: left};
+        </style>`;
+        html+='<div id="ownerInfo">';
+        html+='<h1>'+record.$id.join(' - ')+'</h1>';
+        html+=`
+        <table>
+            <tr>
+                <th>UUID</th>
+                <td>${record._id}</td>
+            </tr>
+            <tr>
+                <th>Revision</th>
+                <td>${record._rev}</td>
+            </tr>
+            <tr>
+                <th>Kind</th>
+                <td>${record.$kind}</td>
+            </tr>
+            <tr>
+                <th>Creation date</th>
+                <td>${new Date(record.$creationDate)}</td>
+            </tr>
+            <tr>
+                <th>Owner</th>
+                <td>${record.$owners[0]}</td>
+            </tr>
+            <tr>
+                <th>Last modification date</th>
+                <td>${new Date(record.$modificationDate)}</td>
+            </tr>
+            <tr>
+                <th>Last modified by</th>
+                <td>${record.$lastModification}</td>
+            </tr>
+             <tr>
+                <th>Other owners</th>
+                <td>${record.$owners.slice(1).join(' - ')}</td>
+            </tr>
+        
+        </table>`;
+        html+='</div>';
+
+
+        UI.dialog(html, {
+            width:800,
+            height:400
+        });
+    }
+
+    return recordInfo;
+
+});
+
+
+
+
+
+
+
+
+
