95080ce24b8cfea6debde26df39c9493a1403fe4
Luc Patiny <luc@patiny.com>
2017-05-05 09:25:48 +0200

    Add conditions for missing fields

--- rest-on-couch/showUserInfo.js
+++ rest-on-couch/showUserInfo.js
@@ -11,42 +11,51 @@ define(['src/util/ui'], function(UI) {
         </style>`;
         html+='<div id="userInfo">';
         html+='<h1>'+user.email+'</h1>';
-        html+=`
-        <table>
-            <tr>
+        html+='<table>';
+        if (user.userId) html+=`<tr>
                 <th>User ID</th>
                 <td>${user.userId}</td>
-            </tr>
-            <tr>
+            </tr>`;
+
+        if (user.firstName) html+=`<tr>
                 <th>Firstname</th>
                 <td>${user.firstName}</td>
-            </tr>
-            <tr>
+            </tr>`;
+
+
+        if (user.lastName) html+=`<tr>
                 <th>Lastname</th>
                 <td>${user.lastName}</td>
-            </tr>
-            <tr>
+            </tr>`;
+
+
+        if (user.groupName) html+=`<tr>
                 <th>Group</th>
                 <td>${user.groupName}</td>
-            </tr>
-            <tr>
+            </tr>`;
+
+        if (user.phoneNumber) html+=`<tr>
                 <th>Phone number</th>
                 <td>${user.phoneNumber}</td>
-            </tr>
-            <tr>
+            </tr>`;
+
+
+        if (user.roomNumber) html+=`<tr>
                 <th>Room number</th>
                 <td>${user.roomNumber}</td>
-            </tr>
-            <tr>
+            </tr>`;
+
+        if (user.postalAddress) html+=`<tr>
                 <th>Room number</th>
                 <td>${user.postalAddress}</td>
-            </tr>
-             <tr>
+            </tr>`;
+
+        if (user.allGroups && Array.isArray(allGroups)) html+=`<tr>
                 <th>Groups</th>
                 <td>${user.allGroups.join('<br>')}</td>
-            </tr>
-        
-        </table>`;
+            </tr>`;
+
+        html+='</table>';
         html+='</div>';
 
 
