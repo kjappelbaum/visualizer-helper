acc01e125b78bf502a9321b5c8bc02429fa1115c
Luc Patiny <luc@patiny.com>
2017-06-07 14:41:00 +0200

    update eln-plugin

--- eln/libs.js
+++ eln/libs.js
@@ -2,4 +2,4 @@
 export {default as OCLE} from 'https://www.lactame.com/lib/openchemlib-extended/1.13.0/openchemlib-extended.js';
 export {default as SD} from 'https://www.lactame.com/lib/spectra-data/3.1.0/spectra-data.min.js';
 export {default as CCE} from 'https://www.lactame.com/lib/chemcalc-extended/1.27.0/chemcalc-extended.js';
-export {default as elnPlugin} from 'https://www.lactame.com/lib/eln-plugin/0.1.2/eln-plugin.js';
+export {default as elnPlugin} from 'https://www.lactame.com/lib/eln-plugin/0.1.3/eln-plugin.js';
