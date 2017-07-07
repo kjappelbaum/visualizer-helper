a2554cf5660e803f782b827cb9fd5a37c268fd7a
Luc Patiny <luc@patiny.com>
2017-07-07 18:00:22 +0200

    fix eslint

--- eln/ExpandableMolecule.js
+++ eln/ExpandableMolecule.js
@@ -14,7 +14,6 @@ class ExpandableMolecule {
         this.options = Object.assign({}, defaultOptions, options);
         this.sample = sample;
         this.molfile = String(this.sample.getChildSync(['$content', 'general', 'molfile']) || '');
-        var start=Date.now();
         this.idCode = OCLE.Molecule.fromMolfile(this.molfile).getIDCode();
         this.expandedHydrogens = false;
         this.jsmeEditionMode = false;
@@ -26,7 +25,6 @@ class ExpandableMolecule {
             // need to compare former oclID with new oclID
             var newMolecule = OCLE.Molecule.fromMolfile(event.target + '');
 
-            var start=Date.now();
             var oclID = newMolecule.getIDCodeAndCoordinates();
             if (oclID.idCode !== this.idCode) {
                 this.idCode = oclID.idCode;
@@ -59,7 +57,7 @@ class ExpandableMolecule {
     }
 
     toggleJSMEEdition() {
-        this.jsmeEditionMode = ! this.jsmeEditionMode;
+        this.jsmeEditionMode = !this.jsmeEditionMode;
         this.expandedHydrogens = false;
         let options = {
             prefs: []
@@ -96,7 +94,6 @@ class ExpandableMolecule {
             var start = Date.now();
             molecule.getCompactCopy().getIDCode();
             let expected = (Date.now() - start) * 4 * molecule.getAllAtoms();
-            console.log(Date.now() - start);
             if (expected > this.maxDiastereotopicCalculationTime) {
                 // eslint-disable-next-line no-console
                 console.log('The diastereotopic calculation is expected to last more than 3s. No way to assign molecule.',
