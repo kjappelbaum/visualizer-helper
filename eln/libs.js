6526cc2fafb20badd77708a30af844cb9e0d0055
Michaël Zasso <targos@protonmail.com>
2017-07-13 14:08:15 +0200

    update eln-plugin

--- eln/libs.js
+++ eln/libs.js
@@ -2,4 +2,4 @@
 export {default as OCLE} from 'https://www.lactame.com/lib/openchemlib-extended/2.4.0/openchemlib-extended.js';
 export {default as SD} from 'https://www.lactame.com/lib/spectra-data/3.1.7/spectra-data.min.js';
 export {default as CCE} from 'https://www.lactame.com/lib/chemcalc-extended/1.27.5/chemcalc-extended.js';
-export {default as elnPlugin} from 'https://www.lactame.com/lib/eln-plugin/0.2.1/eln-plugin.min.js';
+export {default as elnPlugin} from 'https://www.lactame.com/lib/eln-plugin/0.2.2/eln-plugin.min.js';
