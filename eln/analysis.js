7f4448dabc98d99906bbe9ed5969605926bb99ff
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-08-31 09:48:52 +0200

    refactor: update to follow proposed API

--- eln/analysis.js
+++ eln/analysis.js
@@ -1,52 +1,63 @@
-import API from 'src/util/api';
+import UI from 'src/util/ui';
 import Roc from '../rest-on-couch/Roc';
 
-export async function analysisFactory(analysisId, sampleId, options = {}) {
-    const kind = 'analysis';
+const kind = 'analysis';
+
+export async function analysisFactory(sampleId, viewUUID, options = {}) {
     const {
         url = undefined,
         database = 'eln',
-        category = '',
-        description = '',
-        result = [],
-        preference = {}
+        category = 'analysis'
     } = options;
 
     var roc = new Roc({url, database, kind});
-    var existing = (await roc.view('analysisBySampleAndId', {key: [sampleId, analysisId]}))[0];
 
-    var rocOptions = {
-        track: true,
-        varName: 'viewAnalysis'
-    };
-    var analysisRoc;
-    if (existing) {
-        analysisRoc = await roc.document(existing._id, rocOptions);
-    } else {
-        var created = await roc.create({
-            $id: [sampleId, analysisId],
-            $content: {
-                results: [{result, preference}],
-                category,
-                description
-            },
-            $kind: kind
-        });
-        analysisRoc = await roc.document(created._id, rocOptions);
-    }
-
-    var viewAnalysis = new Analysis(roc, analysisRoc);
-    await API.cache(name, viewAnalysis);
-    return viewAnalysis;
+    return new AnalysisManager(roc, sampleId, viewUUID, category);
 }
 
-export class Analysis {
-    constructor(roc, analysisRoc) {
+export class AnalysisManager {
+    constructor(roc, sampleId, viewUUID, category) {
         this.roc = roc;
-        this.analysis = analysisRoc;
+        this.sampleId = sampleId;
+        this.viewUUID = viewUUID;
+        this.category = category;
     }
 
-    async save() {
-        await this.roc.update(this.analysis);
+    async load(label) {
+        var existing = (await this.roc.view('analysisById', {key: [this.sampleId, this.viewUUID, label]}))[0];
+
+        var rocOptions = {
+            track: true,
+            varName: 'viewAnalysis'
+        };
+
+        if (existing) {
+            return this.roc.document(existing._id, rocOptions);
+        } else {
+            UI.showNotification(`Analysis ${label} not found`, 'error');
+            return Promise.reject();
+        }
+    }
+
+    async save(label, analysisResult) {
+        var existing = (await this.roc.view('analysisById', {key: [this.sampleId, this.viewUUID, label]}))[0];
+
+        if (existing) {
+            existing.$content.result = analysisResult;
+            return this.roc.update(existing._id);
+        } else {
+            return this.roc.create({
+                $id: [this.sampleId, this.viewUUID, label],
+                $content: {
+                    category: this.category,
+                    result: analysisResult
+                },
+                $kind: kind
+            });
+        }
+    }
+
+    list() {
+        return this.roc.view('analysisByIdAndCategory', {key: [this.sampleId, this.viewUUID, this.category]});
     }
 }
