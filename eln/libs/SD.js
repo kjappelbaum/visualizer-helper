dc4e3e4029c1275d9698b1e85c1ec6f558438b71
Luc Patiny <luc@patiny.com>
2017-09-14 15:15:52 +0200

    update spectra-data version to 3.1.13

--- eln/libs/SD.js
+++ eln/libs/SD.js
@@ -1,1 +1,1 @@
-export {default} from 'https://www.lactame.com/lib/spectra-data/3.1.7/spectra-data.min.js';
+export {default} from 'https://www.lactame.com/lib/spectra-data/3.1.8/spectra-data.min.js';
