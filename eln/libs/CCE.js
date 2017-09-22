d108c904ff611a90e5466e23d13cd75004ab795d
Luc Patiny <luc@patiny.com>
2017-09-22 14:01:03 +0200

    update chemcalc extended to 1.29.1

--- eln/libs/CCE.js
+++ eln/libs/CCE.js
@@ -1,1 +1,1 @@
-export {default} from 'https://www.lactame.com/lib/chemcalc-extended/1.29.0/chemcalc-extended.js';
+export {default} from 'https://www.lactame.com/lib/chemcalc-extended/1.29.1/chemcalc-extended.js';
