87cb69559824b365c09cd9eccf70bdd3e8e87929
Michaël Zasso <targos@protonmail.com>
2017-09-25 11:59:40 +0200

    update sd to 3.2.3

--- eln/libs/SD.js
+++ eln/libs/SD.js
@@ -1,4 +1,4 @@
 // import * as SD from 'https://77426d8f.ngrok.io/cheminfo-js/spectra/dist/SD.js?noBabel=true';
 
-import * as SD from 'https://www.lactame.com/lib/spectra-data/3.2.2/spectra-data.min.js';
+import * as SD from 'https://www.lactame.com/lib/spectra-data/3.2.3/spectra-data.min.js';
 export {SD as default};
