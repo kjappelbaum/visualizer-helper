10f560f91b329512d9e2783005f7464248890fe1
Luc Patiny <luc@patiny.com>
2017-11-06 18:12:13 +0100

    add second format for acsIR

--- eln/acs/ir.js
+++ eln/acs/ir.js
@@ -1,4 +1,10 @@
-function toHTML(value) {
+function toHTML(value, options = {}) {
+    if (options.parenthesis) return format2(value);
+    return format1(value);
+}
+
+// IR (cm-1): 1955w, 1881w, 1807w, 1614m, 1500S, 1454m
+function format1(value) {
     var acsString = '';
     if (value && value.peak) {
         acsString += 'IR (cm<sup>-1</sup>): ';
@@ -6,5 +12,16 @@ function toHTML(value) {
     }
     return acsString;
 }
+
+// IR (cm-1): 2929 (w), 3521 (w), 3016 (w), 3065 (w), 2853 (w), 1766S, 1495 (w),
+function format2(value) {
+    var acsString = '';
+    if (value && value.peak) {
+        acsString += 'IR (cm<sup>-1</sup>): ';
+        acsString += value.peak.map(a => Math.round(a.wavelength) + (a.kind ? ' (' + a.kind + ')' : '')).join(', ');
+    }
+    return acsString;
+}
+
 
 module.exports = toHTML;
