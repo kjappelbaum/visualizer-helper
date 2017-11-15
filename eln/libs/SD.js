a424418b093e8535b868dfc08f18a993e44bfb7e
Luc Patiny <lpatiny@users.noreply.github.com>
2017-11-15 08:12:42 +0100

    Update SD.js

--- eln/libs/SD.js
+++ eln/libs/SD.js
@@ -1,4 +1,21 @@
 // import * as SD from 'https://77426d8f.ngrok.io/cheminfo-js/spectra/dist/SD.js?noBabel=true';
 
-import * as SD from 'https://www.lactame.com/lib/spectra-data/3.2.4/spectra-data.min.js';
-export {SD as default};
+import * as SDLib from 'https://www.lactame.com/lib/spectra-data/3.2.4/spectra-data.min.js';
+
+const {
+  GUI,
+  NMR,
+  NMR2D,
+  Ranges,
+  SD
+} = SDLib;
+
+
+export {
+  SDLib as default,
+  GUI,
+  NMR,
+  NMR2D,
+  Ranges,
+  SD
+};
