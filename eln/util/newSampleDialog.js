fe728a9924ddaeda6e3914f7abfeba1332912960
stropitek <kostro.d@gmail.com>
2018-03-01 12:48:45 +0100

    script that creates a new sample with groups

---
+++ eln/util/newSampleDialog.js
@@ -0,0 +1,56 @@
+"use strict";
+
+define(["src/util/ui"], function(ui) {
+  return async function createSample(roc, allGroups) {
+    const storageKey = "eln-new-sample-default-groups";
+    var entrysample = { $content: {} };
+    var data = { allGroups };
+    var toFill = { group: {} };
+    const allGroupNames = allGroups.map(g => g.name);
+    let groupPref = localStorage.getItem(storageKey);
+    groupPref = groupPref ? JSON.parse(groupPref) : [];
+    groupPref = _.intersection(allGroupNames, groupPref);
+    groupPref.forEach(group => (toFill.group[group] = true));
+    const result = await ui.form(
+      `
+              <div>
+              <form>
+                  <table>
+                  <tr>
+                  <th align=right>Reference<br><span style='font-size: smaller'>Reaction code</span></th>
+                  <td><input type="text" name="code" pattern="[A-Za-z0-9 .-]{3,}"/></td>
+                  </tr>
+                  <tr>
+                  <th align=right>Batch<br><span style='font-size: smaller'>Fraction</span></th>
+                  <td><input type="text" name="batch" pattern="[A-Za-z0-9 .-]*"/></td>
+                  </tr>
+                  <tr>
+                  <th>Groups</th>
+                  <td>
+                  {% for group in allGroups %}
+                    <div>
+                      <input type="checkbox" name="group.{{ group.name }}" />
+                      <span style="font-weight: bold;">{{group.name}}</span>
+                      <span>{{ group.description }}</span>
+                    </div>
+                  {% endfor %}
+                  </td>
+                  </tr>
+                  </table>
+                  <input type="submit" value="Submit"/>
+              </form>
+              </div>
+          `,
+      toFill,
+      { twig: data }
+    );
+    if (!result || !result.code || result.batch == null) return;
+
+    const selected = Object.keys(result.group).filter(key => result.group[key]);
+
+    entrysample.$id = [result.code, result.batch];
+    entrysample.$owners = selected;
+    localStorage.setItem(storageKey, JSON.stringify(selected));
+    // return roc.create(entrysample);
+  };
+});
