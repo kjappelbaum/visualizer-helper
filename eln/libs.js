76fb45595dbf91a594c9a97a8f863dc172d3587f
Luc Patiny <luc@patiny.com>
2018-03-14 16:00:51 +0100

    add EMDB in libs

--- eln/libs.js
+++ eln/libs.js
@@ -3,5 +3,6 @@ export {default as SD} from './libs/SD';
 export {default as CCE} from './libs/CCE';
 export {default as elnPlugin} from './libs/elnPlugin';
 export {default as Image} from './libs/Image';
+export {default as EMDB} from './libs/EMDB';
 export {parseXY} from './libs/parseXY';
 export {convert} from './libs/jcampconverter';
