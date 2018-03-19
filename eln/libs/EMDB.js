ea3ea94aa4643e991f06e48b3570539dea8b88fa
Luc Patiny <luc@patiny.com>
2018-03-19 07:55:52 +0100

    update emdb to 0.1.0

--- eln/libs/EMDB.js
+++ eln/libs/EMDB.js
@@ -1,1 +1,1 @@
-export {default} from 'https://www.lactame.com/lib/mass-database-manager/0.0.9/mass-database-manager.js';
+export {default} from 'https://www.lactame.com/lib/mass-database-manager/0.1.0/mass-database-manager.js';
