715e106ecfaa01ab937ac19698920e64232d281b
Daniel Kostro <kostro.d@gmail.com>
2018-06-15 14:33:32 +0200

    revert get download url in explodeNucleic

--- eln/nucleic/util.js
+++ eln/nucleic/util.js
@@ -4,19 +4,10 @@ export function explodeNucleic(nucleic) {
   for (let i = 0; i < nucleic.length; i++) {
     const { seq, ...otherFeatures } = nucleic[i];
     for (let j = 0; j < seq.length; j++) {
-      const el = {
+      result.push({
         ...otherFeatures,
         seq: seq[j]
-      };
-
-      if (nucleic[i].dUrl) {
-        Object.defineProperty(el, 'dUrl', {
-          value: nucleic[i].dUrl,
-          enumerable: false,
-          writable: true
-        });
-      }
-      result.push(el);
+      });
     }
   }
   return result;
