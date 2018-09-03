e3ff5ab2417e82f21a7d61dff2fdce0b57a03cea
Luc Patiny <luc@patiny.com>
2018-09-03 15:43:17 +0200

    spectra-data to 3.7.1

--- eln/libs/SD.js
+++ eln/libs/SD.js
@@ -1,6 +1,6 @@
 // import * as SD from 'https://77426d8f.ngrok.io/cheminfo-js/spectra/dist/SD.js?noBabel=true';
 
-import * as SDLib from 'https://www.lactame.com/lib/spectra-data/3.7.0/spectra-data.min.js';
+import * as SDLib from 'https://www.lactame.com/lib/spectra-data/3.7.1/spectra-data.min.js';
 
 const { GUI, NMR, NMR2D, Ranges, SD } = SDLib;
 
