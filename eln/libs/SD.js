cd8bb3ebed240386808f471d2fabefcc60345d3e
Luc Patiny <luc@patiny.com>
2018-09-03 15:19:19 +0200

    change spectra-data to version 3.7.0

--- eln/libs/SD.js
+++ eln/libs/SD.js
@@ -1,21 +1,7 @@
 // import * as SD from 'https://77426d8f.ngrok.io/cheminfo-js/spectra/dist/SD.js?noBabel=true';
 
-import * as SDLib from 'https://www.lactame.com/lib/spectra-data/3.6.1/spectra-data.min.js';
+import * as SDLib from 'https://www.lactame.com/lib/spectra-data/3.7.0/spectra-data.min.js';
 
-const {
-  GUI,
-  NMR,
-  NMR2D,
-  Ranges,
-  SD
-} = SDLib;
+const { GUI, NMR, NMR2D, Ranges, SD } = SDLib;
 
-
-export {
-  SDLib as default,
-  GUI,
-  NMR,
-  NMR2D,
-  Ranges,
-  SD
-};
+export { SDLib as default, GUI, NMR, NMR2D, Ranges, SD };
