09719b26164923e2f5fc5cc709818a3d58f50499
Luc Patiny <luc@patiny.com>
2018-09-20 13:50:24 +0200

    rename EMDB to MolecularFormula

--- eln/libs.js
+++ eln/libs.js
@@ -2,7 +2,7 @@ export { default as OCLE } from './libs/OCLE';
 export { default as SD } from './libs/SD';
 export { default as elnPlugin } from './libs/elnPlugin';
 export { default as Image } from './libs/Image';
-export { default as EMDB } from './libs/EMDB';
+export { default as MolecularFormula } from './libs/MolecularFormula';
 export { default as convertToJcamp } from './libs/convertToJcamp';
 export { parseXY } from './libs/parseXY';
 export { convert } from './libs/jcampconverter';
--- eln/libs/EMDB.js
+++
@@ -1,3 +0,0 @@
-export {
-  default
-} from 'https://www.lactame.com/lib/molecular-formula/0.1.3/molecular-formula.js';
