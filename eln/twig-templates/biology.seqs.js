c616a44a6d1ee94ad11f7722442e0a745fed43c5
Luc Patiny <luc@patiny.com>
2018-09-24 10:34:25 +0200

    add 5prime modification in nucleotides

--- eln/twig-templates/biology.seqs.js
+++ eln/twig-templates/biology.seqs.js
@@ -1,7 +1,6 @@
 // you may use the following page to test:
 // http://www.cheminfo.org/?viewURL=https%3A%2F%2Fcouch.cheminfo.org%2Fcheminfo-public%2Fb9728349aa850f575f594496a2a38233%2Fview.json&loadversion=true&fillsearch=Twig+dynamic+form+experiments
 
-
 module.exports = `
 
 <style>
@@ -24,13 +23,15 @@ module.exports = `
                     <tr data-repeat='seq'>
                         <td>
                             Name: <input data-field='name' size=40>
-                             Type: <select data-field='moleculeType'>
+                            Type: <select data-field='moleculeType'>
                                 <option value=''></option>
                                 <option value='Peptide'>Peptide</option>
                                 <option value='Protein'>Protein</option>
                                 <option value='Antibody'>Antibody</option>
                                 <option value='Monoclonal antibody'>Monoclonal antibody</option>
-                            </select><br>
+                            </select>
+                            
+                            <br>
                             <textarea cols=100 rows=7 data-field='sequence'></textarea>
                         </td>
                     </tr>
@@ -52,6 +53,12 @@ module.exports = `
                                 <option value='DNA'>DNA</option>
                                 <option value='ds-DNA'>ds-DNA</option>
                                 <option value='RNA'>RNA</option>
+                            </select>&nbsp;
+                            5': <select data-field='5prime'>
+                                <option value='Alcohol'>Alcohol</option>
+                                <option value='Monophosphate'>Monophosphate</option>
+                                <option value='Diphosphate'>Diphosphate</option>
+                                <option value='Triphosphate'>Triphosphate</option>
                             </select>
                             - Primer:
                             <input type='checkbox' data-field='primer'>
