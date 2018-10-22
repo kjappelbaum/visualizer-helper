9f340a92719539a5179c4ff5a91a39b2835ff9ba
Luc Patiny <luc@patiny.com>
2018-10-22 15:22:10 +0200

    upgrate molecular formula to 0.3.11

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,3 +1,3 @@
 export {
   default
-} from 'https://www.lactame.com/lib/molecular-formula/0.3.10/molecular-formula.js';
+} from 'https://www.lactame.com/lib/molecular-formula/0.3.11/molecular-formula.js';
