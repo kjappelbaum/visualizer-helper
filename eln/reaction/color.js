1cf99f35e31547e02b022158cafdd4b6d6440d0d
Luc Patiny <luc@patiny.com>
2018-10-27 07:51:25 +0200

    fix . in reaction color status

--- eln/reaction/color.js
+++ eln/reaction/color.js
@@ -2,7 +2,7 @@ const STATUS = [
   { code: 'started', label: 'Started', color: 'rgba(244,204,204,1)' },
   { code: 'finished', label: 'Finished', color: 'rgba(252,229,205,1)' },
   { code: 'worked-up', label: 'Worked up', color: 'rgba(255,242,204,1)' },
-  { code: 'purified', label: 'Purified', color: 'rgba(217.234.211,1)' },
+  { code: 'purified', label: 'Purified', color: 'rgba(217,234,211,1)' },
   { code: 'closed', label: 'Closed', color: 'rgba(206,224,227,1)' }
 ];
 
