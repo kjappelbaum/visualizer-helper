kjappelbaum/visualizer-helper

commit 36975a7d4cb1342975c72af87f082f1d38ddf202

add reaction color getNextStatus

eln/jpaths.js
@@ -43,7 +43,7 @@ jpaths.nucleic = ['$content', 'biology', 'nucleic'];
 jpaths.peptidic = ['$content', 'biology', 'peptidic'];
 jpaths.chromatogram = ['$content', 'spectra', 'chromatogram'];
 jpaths.biology = ['$content', 'biology'];
-jpaths.thermogravimetricAnalysis = [
+jpaths.thermowgravimetricAnalysis = [
   '$content',
   'spectra',
   'thermogravimetricAnalysis'

eln/reaction/__tests__/color.test.js
@@ -7,4 +7,5 @@ test('Reaction status color', () => {
   let form = Color.getForm('closed');
   expect(form.length).toBeGreaterThan(100);
   expect(form).toMatch('<option value="closed" selected>');
+  expect(Color.getNextStatus('worked-up')).toBe('purified');
 });

eln/reaction/color.js
@@ -24,6 +24,16 @@ function getLabel(statusCode) {
   return 'white';
 }
 
+function getNextStatus(statusCode) {
+  for (let i = 0; i < STATUS.length; i++) {
+    let status = STATUS[i];
+    if (status.code === statusCode && i < STATUS.length - 1) {
+      return STATUS[i + 1].code;
+    }
+  }
+  return statusCode;
+}
+
 function getForm(currentStatus) {
   return `
     <style>
@@ -53,5 +63,6 @@ module.exports = {
   STATUS,
   getColor,
   getLabel,
-  getForm
+  getForm,
+  getNextStatus
 };

commit 0f4a5b37d1a910b5487eb65305e4aa37e4049f14

fix bug introduced in 36975a7d4cb1342975c72af87f082f1d38ddf202

eln/jpaths.js
@@ -43,7 +43,7 @@ jpaths.nucleic = ['$content', 'biology', 'nucleic'];
 jpaths.peptidic = ['$content', 'biology', 'peptidic'];
 jpaths.chromatogram = ['$content', 'spectra', 'chromatogram'];
 jpaths.biology = ['$content', 'biology'];
-jpaths.thermowgravimetricAnalysis = [
+jpaths.thermogravimetricAnalysis = [
   '$content',
   'spectra',
   'thermogravimetricAnalysis'