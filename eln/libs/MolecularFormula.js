8e212d4a1b90c54346cae8f2167a9bd2718df1e5
Luc Patiny <luc@patiny.com>
2018-12-07 13:21:26 +0100

    update MolecularFormula to 0.5.0

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,3 +1,3 @@
 export {
   default
-} from 'https://www.lactame.com/lib/molecular-formula/0.4.0/molecular-formula.js';
+} from 'https://www.lactame.com/lib/molecular-formula/0.5.0/molecular-formula.js';
