f952f889317d2c4fb9ea5e9fb1dcbff908136949
Luc Patiny <luc@patiny.com>
2018-12-07 14:42:17 +0100

    update MoleculeFormula to HEAD

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,3 +1,3 @@
 export {
   default
-} from 'https://www.lactame.com/lib/molecular-formula/0.5.1/molecular-formula.js';
+} from 'https://www.lactame.com/lib/molecular-formula/HEAD/molecular-formula.js';
