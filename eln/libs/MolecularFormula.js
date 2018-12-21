066961b1ae2029a88d2b8ed69b03a8e44daede05
Luc Patiny <luc@patiny.com>
2018-12-21 14:05:52 +0100

    change molecular formula to head

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,3 +1,3 @@
 export {
   default
-} from 'https://www.lactame.com/lib/molecular-formula/0.8.0/molecular-formula.js';
+} from 'https://www.lactame.com/lib/molecular-formula/HEAD/molecular-formula.js';
