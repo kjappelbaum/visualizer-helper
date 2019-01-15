b83c3316c8df22e2b0d06b6a9872dc60c8f5ff92
Luc Patiny <luc@patiny.com>
2019-01-15 14:43:41 +0100

    MolecularFormula is on head for testing

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,3 +1,4 @@
 export {
   default
-} from 'https://www.lactame.com/lib/molecular-formula/0.10.2/molecular-formula.min.js';
+} from 'https://www.lactame.com//lib/molecular-formula/HEAD/molecular-formula.js';
+
