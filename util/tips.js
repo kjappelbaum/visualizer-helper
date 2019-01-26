51ab0478f9019f196ca4c1601e7b91d0cf993956
Luc Patiny <luc@patiny.com>
2019-01-26 14:50:54 +0100

    rename title of help to Information about the page

--- util/tips.js
+++ util/tips.js
@@ -95,7 +95,7 @@ define([
             <iframe frameBorder="0" width="100%" height="100%" 
             src="${pagesURL + info._id}">
         `,
-        { width: 900, height: 700, title: 'Did you know ?' }
+        { width: 900, height: 700, title: 'Information about the page' }
       );
     });
 
@@ -115,7 +115,7 @@ define([
                 <iframe frameBorder="0" width="100%" height="100%" 
                 src="${pagesURL + info._id}">
             `,
-            { width: 900, height: 700, title: 'Userguide' }
+            { width: 900, height: 700, title: 'Information about the page' }
           );
         }
       },
