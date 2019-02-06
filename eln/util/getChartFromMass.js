8f179dbb9a2f7784111e90077a64e2a9f26a4859
Luc Patiny <luc@patiny.com>
2019-02-06 08:36:44 +0100

    improve getChartFromMass

--- eln/util/getChartFromMass.js
+++ eln/util/getChartFromMass.js
@@ -1,6 +1,5 @@
 import { convert } from '../libs/jcampconverter';
 import { parseXY } from '../libs/parseXY';
-
 // we extract the data from an object
 // the data may be in experiment.content
 // or in experiment.jcamp.
@@ -15,72 +14,38 @@ export async function getChartFromMass(experiment, options = {}) {
   if (experiment.jcamp) {
     let name =
       options.name || String(experiment.jcamp.filename).match(/([^/]+)\..+/)[1];
-    if (String(experiment.jcamp.encoding) === 'text') {
-      let result = convert(String(experiment.jcamp.content), { xy: true });
-      let points = result.spectra[0].data[0];
-      return {
-        data: [
-          {
-            label: name,
-            x: points.x,
-            y: points.y
-          }
-        ]
-      };
-    } else if (experiment.jcamp.dUrl) {
-      let content = await fetch(String(experiment.jcamp.dUrl), {
-        credentials: 'include'
-      }).then((r) => r.text());
-      let result = convert(content, { xy: true });
-      let points = result.spectra[0].data[0];
-      return {
-        data: [
-          {
-            label: name,
-            x: points.x,
-            y: points.y
-          }
-        ]
-      };
-    } else if (experiment.jcamp.encoding === undefined) {
-      return {}; // no data
-    } else {
-      throw new Error(`unsupported encoding ${experiment.jcamp.encoding}`);
-    }
+    let data = (await experiment.getChild(['jcamp', 'data'])).get();
+    let jcamp = String(data);
+
+    let result = convert(jcamp, { xy: true });
+    let points = result.spectra[0].data[0];
+    return {
+      data: [
+        {
+          label: name,
+          x: points.x,
+          y: points.y
+        }
+      ]
+    };
   } else if (experiment.text) {
     let name =
       options.name || String(experiment.text.filename).match(/([^/]+)\..+/)[1];
-    if (String(experiment.text.encoding) === 'text') {
-      let points = parseXY(String(experiment.text.content), {
-        arrayType: 'xxyy',
-        uniqueX: true
-      });
-      return {
-        data: [
-          {
-            label: name,
-            x: points[0],
-            y: points[1]
-          }
-        ]
-      };
-    } else if (experiment.text.dUrl) {
-      let content = await fetch(String(experiment.text.dUrl), {
-        credentials: 'include'
-      }).then((r) => r.text());
-      let points = parseXY(content, { arrayType: 'xxyy', uniqueX: true });
-      return {
-        data: [
-          {
-            label: name,
-            x: points[0],
-            y: points[1]
-          }
-        ]
-      };
-    } else {
-      throw new Error(`unsupported encoding ${experiment.text.encoding}`);
-    }
+    let data = (await experiment.getChild(['text', 'data'])).get();
+    let text = String(data);
+    let points = parseXY(String(text), {
+      arrayType: 'xxyy',
+      uniqueX: true
+    });
+    return {
+      data: [
+        {
+          label: name,
+          x: points[0],
+          y: points[1]
+        }
+      ]
+    };
   } else {
     throw new Error('the file should be a jcamp or text');
   }
