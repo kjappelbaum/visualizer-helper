28356f3b971981dbbe6c277ae909c0d308914974
Luc Patiny <luc@patiny.com>
2019-03-20 16:49:31 +0100

    add helper to store analytical results

---
+++ rest-on-couch/UserAnalysisResults.js
@@ -0,0 +1,58 @@
+define(['../util/getViewInfo'], function (getViewInfo) {
+  class UserAnalysisResults {
+    constructor(roc, sampleID) {
+      this.roc = roc;
+      this.sampleID = sampleID;
+      this.viewID = undefined;
+    }
+
+    setSampleID(sampleID) {
+      this.sampleID = sampleID;
+    }
+
+    /**
+     * Retrieve all the analytical results for a sample in a view
+     * @param {string} key
+     */
+    async getRecords(key) {
+      this.viewID = this.viewID || (await getViewInfo())._id;
+      var user = await this.roc.getUser();
+      if (!user || !user.username) return undefined;
+      let options = key
+        ? {
+          key: [
+            user.username,
+            ['userAnalysisResults', this.viewID, this.sampleID, key]
+          ]
+        }
+        : {
+          startkey: [
+            user.username,
+            ['userAnalysisResults', this.viewID, this.sampleID, '\u0000']
+          ],
+          endkey: [
+            user.username,
+            ['userAnalysisResults', this.viewID, this.sampleID, '\uffff']
+          ]
+        };
+      var entries = await this.roc.view('entryByOwnerAndId', options);
+      return entries;
+    }
+
+    async set(key, value) {
+      this.viewID = this.viewID || (await getViewInfo())._id;
+      let record = (await this.getRecords(key))[0];
+      if (record) {
+        record.$content = value;
+        return this.roc.update(record);
+      } else {
+        return this.roc.create({
+          $id: ['userAnalysisResults', this.viewID, this.sampleID, key],
+          $content: value,
+          $kind: 'userAnalysisResults'
+        });
+      }
+    }
+  }
+  return UserAnalysisResults;
+});
