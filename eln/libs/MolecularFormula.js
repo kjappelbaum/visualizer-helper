8ae63eef225edbb769f1d0964bdc1766153a5dca
Luc Patiny <luc@patiny.com>
2019-09-27 10:55:07 +0200

    chore(MolecularFormula): update to 0.36.2

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,4 +1,4 @@
 export {
   default
-} from 'https://www.lactame.com//lib/molecular-formula/0.36.1/molecular-formula.js';
+} from 'https://www.lactame.com//lib/molecular-formula/0.36.2/molecular-formula.js';
 // } from 'http://localhost:9898/cheminfo-js/molecular-formula/dist/molecular-formula.js';
