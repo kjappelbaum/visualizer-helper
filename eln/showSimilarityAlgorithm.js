5ee4f864073e574deff5a76b2cebbe0c204792c7
Luc Patiny <luc@patiny.com>
2019-09-30 16:33:11 +0200

    add intersection

--- eln/showSimilarityAlgorithm.js
+++ eln/showSimilarityAlgorithm.js
@@ -153,6 +153,15 @@ let html = `
         />
       </td>
     </tr>
+ 
+    <tr>
+      <td>intersection<br><i>Requires normalization</i></td>
+      <td style="color:darkgreen;">✔</td>
+      <td style="color:darkred;">✘</td>
+      <td>
+      <img src="https://tex.cheminfo.org/?tex=d(p,q)=1-\sum\limits_{i=1}^{n}min(p_i,q_i)">
+      </td>
+    </tr>
 
     <tr>
       <td>jaccard</td>
