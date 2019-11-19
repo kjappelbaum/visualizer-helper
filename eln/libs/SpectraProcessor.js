669b45544f7bdc530c4279edee5b5d990cc9c035
Luc Patiny <luc@patiny.com>
2019-11-19 12:03:12 +0100

    chore: update spectra-processor to 0.21.0

--- eln/libs/SpectraProcessor.js
+++ eln/libs/SpectraProcessor.js
@@ -1,4 +1,2 @@
-export {
-  SpectraProcessor
-} from 'https://www.lactame.com/lib/spectra-processor/0.20.0/spectra-processor.js';
+export { SpectraProcessor } from 'https://www.lactame.com/lib/spectra-processor/0.21.0/spectra-processor.js';
 // from 'http://localhost:9898/cheminfo-js/spectra-processor/dist/spectra-processor.js';
