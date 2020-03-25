8a3894390b5aa28f0b55ab5e10e8ef2eb0c86f04
Luc Patiny <luc@patiny.com>
2020-03-25 09:07:18 +0100

    chore: update SpectraProcessor to 1.1.2

--- eln/libs/SpectraProcessor.js
+++ eln/libs/SpectraProcessor.js
@@ -1,2 +1,2 @@
-export { SpectraProcessor } from 'https://www.lactame.com/lib/spectra-processor/0.22.0/spectra-processor.js';
+export { SpectraProcessor } from 'https://www.lactame.com/lib/spectra-processor/1.1.2/spectra-processor.min.js';
 // from 'http://localhost:9898/cheminfo-js/spectra-processor/dist/spectra-processor.js';
