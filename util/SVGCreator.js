66c14041bf62ca99dd28e496c78c4d677ddf8af6
Luc Patiny <luc@patiny.com>
2020-08-10 14:37:08 +0200

    feat: improve SVGCreator

--- util/SVGCreator.js
+++ util/SVGCreator.js
@@ -2,49 +2,88 @@ class SVGCreator {
   constructor(height, width) {
     this.height = height;
     this.width = width;
-    this.elements = [];
+    this.children = [];
+    this.scripts = [];
   }
 
-  add(kind, inner, options = {}) {
-    if (typeof inner === 'object') {
-      options = inner;
-      inner = '';
-    }
-    if (inner) {
-      this.elements.push({
-        kind,
-        inner: inner,
-        attributes: options,
-      });
-    } else {
-      this.elements.push({
-        kind,
-        attributes: options,
-      });
-    }
+  addScript(script) {
+    this.script.push(script);
+  }
+
+  addText(text, attributes = {}) {
+    return addText(this.children, text, attributes);
+  }
+
+  add(kind, attributes = {}) {
+    return add(this.children, kind, attributes);
   }
 
   toSVG() {
     let content = `<svg width="${this.width}" height="${this.height}">`;
-    for (let element of this.elements) {
-      let attributes = Object.keys(element.attributes)
-        .map(
-          (key) =>
-            `${key.replace(
-              /([A-Z])/g,
-              (match) => '-' + match.toLowerCase(),
-            )}="${element.attributes[key]}"`,
-        )
-        .join(' ');
-      if (element.inner) {
-        content += `<${element.kind} ${attributes}>${element.inner}</${element.kind}>`;
-      } else {
-        content += `<${element.kind} ${attributes} />`;
-      }
-    }
+    content += getChildrenSVG(this.children);
     content += '</svg>';
     return content;
   }
+}
+
+function getChildrenSVG(children) {
+  let content = '';
+  for (let element of children) {
+    let attributes = Object.keys(element.attributes)
+      .map(
+        (key) =>
+          `${key.replace(/([A-Z])/g, (match) => '-' + match.toLowerCase())}="${
+            element.attributes[key]
+          }"`,
+      )
+      .join(' ');
+    if (element.children.length > 0) {
+      content += `<${element.kind} ${attributes}>${getChildrenSVG(
+        element.children,
+      )}</${element.kind}>`;
+    } else if (element.innerHTML) {
+      console.log(element.innerHTML);
+      content += `<${element.kind} ${attributes}>${element.innerHTML}</${element.kind}>`;
+    } else {
+      content += `<${element.kind} ${attributes} />`;
+    }
+  }
+  return content;
+}
+
+class Element {
+  constructor(kind = '', innerHTML = '', attributes = {}) {
+    this.children = [];
+    this.kind = kind;
+    this.attributes = attributes;
+    this.innerHTML = innerHTML;
+  }
+
+  addText(text, attributes = {}) {
+    return addText(this.children, text, attributes);
+  }
+
+  add(kind, attributes = {}) {
+    return add(this.children, kind, attributes);
+  }
+}
+
+function encodeText(string) {
+  return string.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
+    return '&#' + i.charCodeAt(0) + ';';
+  });
+}
+
+function addText(children, text, attributes = {}) {
+  let element = new Element('text', encodeText(text), attributes);
+  children.push(element);
+  return element;
+}
+
+function add(children, kind, attributes = {}) {
+  let element = new Element(kind, '', attributes);
+  children.push(element);
+  return element;
 }
 
 module.exports = SVGCreator;
