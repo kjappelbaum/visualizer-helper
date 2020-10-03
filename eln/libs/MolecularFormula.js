a3d64b8dd982bc0ec0f8e3847d6c8dbd5c84b31b
Luc Patiny <luc@patiny.com>
2020-10-03 07:44:33 +0200

    chore: update MolecularFormula to 0.54.0

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,2 +1,2 @@
-export { default } from '../../../../../../../../lib/molecular-formula/0.53.0/molecular-formula.min';
+export { default } from '../../../../../../../../lib/molecular-formula/0.54.0/molecular-formula.min';
 // } from 'http://localhost:9898/cheminfo-js/molecular-formula/dist/molecular-formula.js';
