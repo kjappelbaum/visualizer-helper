72aaed24e2cdad192150a23c4b3b4d52ed3570d3
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2020-10-07 10:58:49 +0200

    feat: add isotherm support (#30)
    * feat: add isotherm support
    
    * chore: fixed small typo

--- eln/Sample.js
+++ eln/Sample.js
@@ -286,6 +286,7 @@ Your local changes will be lost.</p>`;
         droppedChrom: 'chromatogram',
         droppedCV: 'cyclicVoltammetry',
         droppedTGA: 'thermogravimetricAnalysis',
+        droppedIsotherm: 'isotherm',
         droppedDSC: 'differentialScanningCalorimetry',
         droppedXray: 'xray',
         droppedXRD: 'xrd',
@@ -313,9 +314,9 @@ Your local changes will be lost.</p>`;
             'Chromatogram LC, GC, LC/MS, GC/MS (csv, tsv, txt, jcamp, pdf, netcdf, xml)',
           thermogravimetricAnalysis:
             'Thermogravimetric Analysis (csv, tsv, txt, jcamp)',
-          xrd: 'Powerder XRD Analysis (csv, tsv, txt, jcamp)',
+          xrd: 'Powder XRD Analysis (csv, tsv, txt, jcamp)',
           xps: 'XPS (csv, tsv, txt, jcamp)',
-          isotherm: 'Isotherm (csv, tsv, txt, jcamp)',
+          isotherm: 'Isotherm (csv, tsv, txt, jcamp, xls)',
           cyclicVoltammetry: 'Cyclic voltammetry (csv, tsv, txt, jcamp, pdf)',
           differentialScanningCalorimetry:
             'Differential Scanning Calorimetry (csv, tsv, txt, jcamp)',
@@ -452,6 +453,11 @@ Your local changes will be lost.</p>`;
           type: 'Differentical scanning calorimetry',
           xUnit: 'I [mA]',
           yUnit: 'Ewe [V]',
+        },
+        isotherm: {
+          type: 'Isotherm',
+          xUnit: ['p/p0', 'p / kPa'],
+          yUnit: ['excess adsorption mmol/g', 'adsorbed volume cm3/g'],
         },
         xrd: {
           type: 'X-ray powder diffraction',
