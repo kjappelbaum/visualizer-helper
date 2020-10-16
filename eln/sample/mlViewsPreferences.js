d06aece76760b0c6b1c1d6e1c92e24f0e97e7e4e
Luc Patiny <luc@patiny.com>
2020-10-16 16:24:28 +0200

    feat: check if original are there in mlViewsPreferences

--- eln/sample/mlViewsPreferences.js
+++ eln/sample/mlViewsPreferences.js
@@ -39,76 +39,82 @@ export const style = `
 </style>`;
 
 export const dataNormalization = `
-<h1>Data normalization</h1>
-<table>
-    <tr>
-        <th align="left">Range:</th>
-        <td>
-            from: <input type="number" name="normalization.from" step="any"> - 
-            to: <input type="number" name="normalization.to" step="any">
-        </td>
-    </tr>
-    <tr>
-        <th align="left">Nb points:</th>
-        <td>
-            <input type='number' name='normalization.numberOfPoints' size="6">
-        </td>
-    </tr>
-    <tr>
-        <th align="left">Filters</th>
-        <td>
-            <table>
-                <tr>
-                    <th></th><th></th>
-                    <th>Name</th>
-                    <th>Options</th>
-                </tr>
-                <tr data-repeat='normalization.filters'>
-                    <td>
-                        <select data-field='name'>
-                            <option value=""></option>
-                            <option value="centerMean">Center Mean</option>
-                            <option value="scaleSD">Divide by SD</option>
-                            <option value="rescale">Rescale (0 to 1)</option>
-                            <option value="normalize">Normalize (sum to 1)</option>
-                        </select>
-    
-                    </td>
-                    <td><input type='number' data-field='options' size="5"></td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <th align="left">Exclusions</th>
-        <td>
-            <table>
-                <tr>
-                    <th></th><th></th>
-                    <th>From</th>
-                    <th>To</th>
-                </tr>
-                <tr data-repeat='normalization.exclusions'>
-                    <td><input type='number' data-field='from' size="5"></td>
-                    <td><input type='number' data-field='to' size="5"></td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+{% if memoryInfo.keepOriginal %}
+    <h1>Data normalization</h1>
+    <table>
+        <tr>
+            <th align="left">Range:</th>
+            <td>
+                from: <input type="number" name="normalization.from" step="any"> - 
+                to: <input type="number" name="normalization.to" step="any">
+            </td>
+        </tr>
+        <tr>
+            <th align="left">Nb points:</th>
+            <td>
+                <input type='number' name='normalization.numberOfPoints' size="6">
+            </td>
+        </tr>
+        <tr>
+            <th align="left">Filters</th>
+            <td>
+                <table>
+                    <tr>
+                        <th></th><th></th>
+                        <th>Name</th>
+                        <th>Options</th>
+                    </tr>
+                    <tr data-repeat='normalization.filters'>
+                        <td>
+                            <select data-field='name'>
+                                <option value=""></option>
+                                <option value="centerMean">Center Mean</option>
+                                <option value="scaleSD">Divide by SD</option>
+                                <option value="rescale">Rescale (0 to 1)</option>
+                                <option value="normalize">Normalize (sum to 1)</option>
+                            </select>
+        
+                        </td>
+                        <td><input type='number' data-field='options' size="5"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <th align="left">Exclusions</th>
+            <td>
+                <table>
+                    <tr>
+                        <th></th><th></th>
+                        <th>From</th>
+                        <th>To</th>
+                    </tr>
+                    <tr data-repeat='normalization.exclusions'>
+                        <td><input type='number' data-field='from' size="5"></td>
+                        <td><input type='number' data-field='to' size="5"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+{% endif %}
 `;
 
 export const spectraDisplay = `
 <h1>Spectra display preferences</h1>
 
 <table>
-    <tr>
-        <th>Display:</th>
-        <td>
-            <input type='radio' name='display.original' value='true'> Original data
-            <input type='radio' name='display.original' value='false'> Normalized data
-        </td>
-    </tr>
+    {% if memoryInfo.keepOriginal %}
+        <tr>
+            <th>Display:</th>
+            <td>
+                <input type='radio' name='display.original' value='true'> Original data
+                <input type='radio' name='display.original' value='false'> Normalized data
+            </td>
+        </tr>
+    {% else %}
+        <input type='hidden' name='display.original' value='false'>
+    {% endif %}
     <tr>
         <th>Box-plot shadow</th>
         <td>
