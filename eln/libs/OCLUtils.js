e3be722711b9ddb9887462521081212a3696cb61
Luc Patiny <luc@patiny.com>
2021-05-12 06:47:11 +0200

    chore: update OCLUtils to 1.2.0

--- eln/libs/OCLUtils.js
+++ eln/libs/OCLUtils.js
@@ -1,4 +1,4 @@
-import * as OCLUtils from '../../../../../../../../lib/openchemlib-utils/1.1.0/openchemlib-utils';
+import * as OCLUtils from '../../../../../../../../lib/openchemlib-utils/1.2.0/openchemlib-utils';
 import OCL from 'openchemlib/openchemlib-core';
 
 export { OCLUtils, OCL };
