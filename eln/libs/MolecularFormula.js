fbbcb550ed0bcd90887bdb52c4f40ad017e95e5d
Luc Patiny <luc@patiny.com>
2021-05-28 13:55:16 +0200

    fix: update mass-tools for Si unsaturation

--- eln/libs/MolecularFormula.js
+++ eln/libs/MolecularFormula.js
@@ -1,2 +1,2 @@
-export { default } from '../../../../../../../../lib/mass-tools/0.61.1/mass-tools.min';
+export { default } from '../../../../../../../../lib/mass-tools/0.61.2/mass-tools.min';
 // } from 'http://localhost:9898/cheminfo-js/molecular-formula/dist/molecular-formula.js';
