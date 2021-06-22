e2ec998b61d2c5624d6882a6fc86ba31e60673d2
Luc Patiny <luc@patiny.com>
2021-06-22 14:39:07 +0200

    chore: fix docs/docs to docs/eln

--- util/docs.js
+++ util/docs.js
@@ -1,7 +1,7 @@
 define(['src/util/ui', './getViewInfo'], function (UI, getViewInfo) {
   let baseUrl = require.s.contexts._.config.baseUrl;
 
-  let pagesURL = baseUrl + '../../docs/docs/uuid/';
+  let pagesURL = `${baseUrl}../../docs/eln/uuid/`;
 
   async function addPageHelp(options = {}) {
     const { iconSize = 'fa-3x' } = options;
